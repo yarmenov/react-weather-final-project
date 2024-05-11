e99fce61a0e772c7fdd329f4cb45aaa4087f02cf
yana <yarmenova@gmail.com>
2024-05-11 20:35:42 +0100

    Added the Github url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Yana Armenova and is open-sourced on {""}
+        <a
+          href="https://github.com/yarmenov/react-weather-final-project.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
