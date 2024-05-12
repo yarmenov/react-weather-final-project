4080c9f81a68c3c026c4bf65b4af29acf9cd1d84
yana <yarmenova@gmail.com>
2024-05-12 20:26:38 +0100

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Yana Armenova and is open-sourced on {""}
-        <a
-          href="https://github.com/yarmenov/react-weather-final-project.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by Yana Armenova and is open-sourced on {""}
+          <a
+            href="https://github.com/yarmenov/react-weather-final-project.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
