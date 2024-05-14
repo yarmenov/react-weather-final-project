bcfc32ec8ff225c0fa63f74243ec2e2ec0eeb5e2
yana <yarmenova@gmail.com>
2024-05-14 12:53:57 +0100

    added css

--- src/Weather.js
+++ src/Weather.js
@@ -4,16 +4,21 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <div classnname="row">
+        <div className="row">
           <div className="col-9">
             <input
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +28,17 @@ export default function Weather() {
         <li>Sunday, 9pm</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="Mostly cloudy"
-          />{" "}
-          6℃
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+              alt="Mostly cloudy"
+              className="float-left"
+            />{" "}
+            <span className="temperature">6</span>
+            <span className="unit">℃</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
